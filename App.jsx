539a52d2c9679ce767b6904deef0ed43132271c6
saniyasaniyayv-oss <saniyasaniyayv@gmail.com>
2025-10-27 11:08:11 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductCatalog from "./ProductCatalog";
+
+function App() {
+  return (
+    <div>
+      <ProductCatalog />
+    </div>
+  );
+}
+
+export default App;
